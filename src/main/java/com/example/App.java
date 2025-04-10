4801e10157142757d29bf77dc2ebf45307fc4527
Mohan <mohanbodke2003@gmail.com>
2025-04-10 17:59:03 +0530

    Initial Maven project with JUnit test

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+    }
+
+    public static String greet() {
+        return "Hello from Jenkins!";
+    }
+}
+
+
+// package com.example;
+
+// /**
+//  * Hello world!
+//  *
+//  */
+// public class App 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World!" );
+//     }
+// }
